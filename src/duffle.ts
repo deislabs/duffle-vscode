3b8891df12473e183917ba9a5012548ff48dcb01
Radu Matei <matei.radu94@gmail.com>
2018-08-17 01:30:25 +0300

    Use async/await for commands

--- src/duffle.ts
+++ src/duffle.ts
@@ -1,36 +1,36 @@
 import * as vscode from 'vscode';
 
 export interface Duffle {
-    init(): Promise<void>;
-    build(): Promise<void>;
-    push(): Promise<void>;
-    import(): Promise<void>;
-    export(): Promise<void>;
-    run(): Promise<void>;
+    init(): Promise<any>;
+    build(): Promise<any>;
+    push(): Promise<any>;
+    import(): Promise<any>;
+    export(): Promise<any>;
+    run(): Promise<any>;
 }
 
 export class DuffleImpl implements Duffle {
-    init(): Promise<void> {
-        return new Promise<void>(() => vscode.window.showInformationMessage('duffle init'));
+    async init(): Promise<any> {
+        return await vscode.window.showInformationMessage('duffle init');
     }
 
-    build(): Promise<void> {
-        return new Promise<void>(() => vscode.window.showInformationMessage('duffle build'));
+    async build(): Promise<any> {
+        return await vscode.window.showInformationMessage('duffle build');
     }
 
-    push(): Promise<void> {
-        return new Promise<void>(() => vscode.window.showInformationMessage('duffle push'));
+    async push(): Promise<any> {
+        return await vscode.window.showInformationMessage('duffle push');
     }
 
-    import(): Promise<void> {
-        return new Promise<void>(() => vscode.window.showInformationMessage('duffle import'));
+    async import(): Promise<any> {
+        return await vscode.window.showInformationMessage('duffle import');
     }
 
-    export(): Promise<void> {
-        return new Promise<void>(() => vscode.window.showInformationMessage('duffle export'));
+    async export(): Promise<any> {
+        return await vscode.window.showInformationMessage('duffle export');
     }
 
-    run(): Promise<void> {
-        return new Promise<void>(() => vscode.window.showInformationMessage('duffle run'));
+    async run(): Promise<any> {
+        return await vscode.window.showInformationMessage('duffle run');
     }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -4,17 +4,17 @@ import * as vscode from 'vscode';
 import { Duffle, DuffleImpl } from './duffle';
 
 
-var duffle: Duffle = new DuffleImpl();
+const duffle: Duffle = new DuffleImpl();
 
 export function activate(context: vscode.ExtensionContext) {
 
     const subscriptions = [
-        vscode.commands.registerCommand('extension.duffleInit', () => duffle.init()),
-        vscode.commands.registerCommand('extension.duffleBuild', () => duffle.build()),
-        vscode.commands.registerCommand('extension.dufflePush', () => duffle.push()),
-        vscode.commands.registerCommand('extension.duffleImport', () => duffle.import()),
-        vscode.commands.registerCommand('extension.duffleExport', () => duffle.export()),
-        vscode.commands.registerCommand('extension.duffleRun', () => duffle.run()),
+        vscode.commands.registerCommand('extension.duffleInit', async () => await duffle.init()),
+        vscode.commands.registerCommand('extension.duffleBuild', async () => await duffle.build()),
+        vscode.commands.registerCommand('extension.dufflePush', async () => await duffle.push()),
+        vscode.commands.registerCommand('extension.duffleImport', async () => await duffle.import()),
+        vscode.commands.registerCommand('extension.duffleExport', async () => await duffle.export()),
+        vscode.commands.registerCommand('extension.duffleRun', async () => await duffle.run()),
     ];
 
     subscriptions.forEach((element) => context.subscriptions.push(element));
