84b4fa9479c50d95a4eaf6425f43f6d392edd060
Ivan Towlson <itowlson@microsoft.com>
2018-09-19 15:25:28 +1200

    Add parameters to duffle.toml (using preliminary TOML syntax)

--- src/commands/exposeparameter.ts
+++ src/commands/exposeparameter.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 
 import * as symbols from '../utils/symbols';
 import { isParameterDefinition } from '../utils/arm';
+import * as duffleTOML from '../duffle/toml';
+import * as edit from '../utils/edit';
 
 export async function exposeParameter(): Promise<void> {
     const editor = vscode.window.activeTextEditor;
@@ -38,10 +40,51 @@ export async function exposeParameter(): Promise<void> {
         return;
     }
 
-    // TODO: where do we add parameters to the authoring inputs?
-    console.log(parameterNameToExpose);
-    console.log(parameterToExpose);
+    const tomlPath = await duffleTOML.locate();
+    if (!tomlPath) {
+        return;  // already showed error message
+    }
 
-    // TODO: open the duffle.toml (or whatever) file with the newly added parameter
-    // selected / positioned
+    const document = await vscode.workspace.openTextDocument(tomlPath);
+
+    const text = document.getText();
+    const hasThisParameter = text.indexOf(`[parameters.${parameterNameToExpose}]`) >= 0;
+
+    if (hasThisParameter) {
+        vscode.window.showErrorMessage(`Parameter ${parameterNameToExpose} is already defined in duffle.toml.`);
+        return;
+    }
+
+    const parametersSectionIndex = text.indexOf('[parameters]');
+    const needsParametersSection = parametersSectionIndex < 0;
+
+    const parameterTOML = makeParameterTOML(parameterNameToExpose, parameterToExpose);
+    if (needsParametersSection) {
+        parameterTOML.unshift("[parameters]");
+    }
+
+    const after = needsParametersSection ? 'at-end' : document.positionAt(parametersSectionIndex).line;
+    const e = edit.insertLines(document, after, parameterTOML);
+    edit.applyEdits(e);
+    edit.show(e);
+}
+
+function makeParameterTOML(name: string, definition: any): string[] {
+    const lines = [
+        `[parameters.${name}]`,
+        `type = "${definition.type}"`
+    ];
+    const quote = (v: any) => (definition.type === 'string') ? `"${v}"` : v;
+    if (definition.defaultValue !== undefined) {
+        lines.push(`defaultValue = ${quote(definition.defaultValue)}`);
+    }
+    if (definition.allowedValues !== undefined) {
+        lines.push(`allowedValues = [${definition.allowedValues.map(quote).join(', ')}]`);
+    }
+    for (const numericProp of ['minValue', 'maxValue', 'minLength', 'maxLength']) {
+        if (definition[numericProp] !== undefined) {
+            lines.push(`${numericProp} = ${definition[numericProp]}`);
+        }
+    }
+    return lines.map((l) => '    ' + l);
 }
